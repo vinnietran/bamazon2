55c872a6593b47c33b353a3cb95b7d210a300f4a
VinTran <tranquillo.vinnie@gmail.com>
2019-04-12 16:27:16 -0400

    working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,42 +14,100 @@ var connection = mysql.createConnection({
   // Your password
   database: "bamazon",
   password: "farmboy1"
- 
+
 });
 
 
-connection.connect(function(err){
-    console.log("Connected as id: " + connection.threadId);
-    afterConnection(questions)
+connection.connect(function (err) {
+  console.log("Connected as id: " + connection.threadId);
 })
 
-function afterConnection(questions) {
-    connection.query("SELECT item_id, product_name, price FROM product", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    })
-    questions();
-  }
 
-function questions(){
+showStore();
+
+//function to display first 2 questions
+function questions() {
   inquirer
-  .prompt([
-    // Here we create a basic text prompt.
-    {
-      type: "input",
-      message: "Please enter the product id that you would like to purchase",
-      name: "product"
-    },
-
-    {
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter the product id that you would like to purchase",
+        name: "product",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
         type: "input",
         message: "How many would like like to purchase?",
-        name: "product"   
-    }
-])
+        name: "quantity",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function updateInventory(inquirerResponse) {
+      var userOrder = parseInt(inquirerResponse.quantity);
+      var item = parseInt(inquirerResponse.product);
+
+      connection.query("SELECT stock_quantity, product_name, price FROM product WHERE ?",
+        [{ item_id: inquirerResponse.product }],
+        function (err, res) {
+
+          var currentInv = parseInt(res[0].stock_quantity);
+          var productName = res[0].product_name;
+          var price = parseInt(res[0].price);
+
+
+          if (err) throw err;
+
+          if (parseInt(currentInv) < parseInt(userOrder)) {
+            console.log("INSUFFICIENT QUANTITY! ORDER LESS!")
+            connection.end();
+          }
+          else {
+            var orderTotal = userOrder * price;
+            var newQuant = parseInt(currentInv) - parseInt(userOrder);
+            connection.query("UPDATE product SET ? WHERE ?",
+              [
+                {
+                  stock_quantity: newQuant
+                },
+                {
+                  item_id: item
+                }
+              ],
+              function (error) {
+                if (error) throw err;
+                console.log("THANK YOU FOR YOUR ORDER!!!! \n",
+                  "You have ordered " + userOrder + " " + productName + "s\n",
+                  "The total for your order is $" + orderTotal);
+              }
+            );
+            connection.end();
+          }
+        })
+    })
 }
-// .then (function(inquirer){
+
+function showStore(){
+  connection.query("SELECT item_id, product_name, price, stock_quantity FROM product",
+        function (err, res) {
+          if (err) throw err;
+          console.log(res);
+          for (var i = 0; i < res.length; i++){
+            
+          }
+
+        })
+        setTimeout(function(){questions();}, 1200)
+      }
 
 
-// })
+
